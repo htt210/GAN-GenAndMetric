8c18088cef6e8f7507ac8b66b4b599b68c1db652
htt210 <hoangthanhtung53ca@gmail.com>
2019-11-29 15:44:22 +1100

    running

--- runCIFAR10_GAN0GP.sh
+++ runCIFAR10_GAN0GP.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 100 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 1 -fake_weight 1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 100 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 1 -fake_weight 1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 100 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 1 -fake_weight 1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 100 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 1 -fake_weight 1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 100 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 1 -fake_weight 1
