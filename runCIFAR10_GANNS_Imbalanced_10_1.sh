30c9a4f3431d52fcd5d20ccea69a061e43156b80
htt210 <hoangthanhtung53ca@gmail.com>
2019-11-29 15:47:34 +1100

    running

--- runCIFAR10_GANNS_Imbalanced_10_1.sh
+++ runCIFAR10_GANNS_Imbalanced_10_1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1
-python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1
-python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1
-python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1
-python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1 -device $1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1 -device $1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1 -device $1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1 -device $1
+python MainImage.py -dataset cifar10 -ndf 64 -loss gan -arch dcgan -ngf 64 -gp_weight 0 -gp_center 0 -niter 50001 -log_interval 1000 -lrg 2e-4 -lrd 2e-4 -batch_size 64 -real_weight 10 -fake_weight 1 -device $1
