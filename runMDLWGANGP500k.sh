72d7e1b58426654b5e7214668de53231acdeadfb
htt210 <hoangthanhtung53ca@gmail.com>
2020-01-14 17:13:32 +1100

    add run 500k files

---
+++ runMDLWGANGP500k.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+
