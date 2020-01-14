66e6f879bda417b83b9c2662604a7e4a763da86d
htt210 <hoangthanhtung53ca@gmail.com>
2020-01-14 14:04:06 +1100

    clean up

--- runMDL0GP.sh
+++ runMDL0GP.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
-python MainMDLMLP.py
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
+python MainMDLMLP.py -device $1
--- runMDLWGANGP.sh
+++ runMDLWGANGP.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
-python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -device $1 -gp_center 1 -gp_weight 10 -loss wgan
 
