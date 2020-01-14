4b52ff4bc12fc8e9d764f0a8ecb3e973cc9279bb
htt210 <hoangthanhtung53ca@gmail.com>
2020-01-14 13:45:32 +1100

    clean up

---
+++ runMDLWGAN0GP.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
+python MainMDLMLP.py -gp_center 0 -gp_weight 10 -loss wgan
