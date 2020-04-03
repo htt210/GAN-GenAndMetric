b657949d4adb24d7ea3b5e692f3f1f38338068fc
htt210 <hoangthanhtung53ca@gmail.com>
2020-04-04 00:10:06 +1100

    add run files

---
+++ runMDLMLPR1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_inter 1. -gp_weight 100 -device $1
