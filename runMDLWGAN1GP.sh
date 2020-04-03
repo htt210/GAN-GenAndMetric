0fe953b9f9c76dbf6c4ba76c426daad5e2dbbbdc
htt210 <hoangthanhtung53ca@gmail.com>
2020-04-04 00:14:33 +1100

    add run files

---
+++ runMDLWGAN1GP.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
+python MainMDLMLP.py -gp_center 1 -gp_weight 10 -loss wgan -device $1
