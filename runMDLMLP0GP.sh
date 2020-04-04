0870b2d78353e87e2b7f1ff2a1d4822ccc09118a
htt210 <hoangthanhtung53ca@gmail.com>
2020-04-04 18:21:55 +1100

    add run files

--- runMDLMLP0GP.sh
+++ runMDLMLP0GP.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
-python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
+python MainMDLMLP.py -gp_center 0 -gp_weight 100 -device $1 -niters 500001
